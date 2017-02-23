04b8b90da184bb7d2fe74e3080675b3b7cfae4aa
muhsinking <muhsinking@gmail.com>
2017-02-23 09:05:46 -0800

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TileCombat
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		SpriteClass tile;
+		float screenHeight;
+		float screenWidth;
+
+		float screenscale;
+		
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			if (!graphics.IsFullScreen)
+				graphics.ToggleFullScreen();		}
+
+
+		protected override void Initialize()
+		{
+			screenscale = 2880f / 1440f;
+
+			//screenWidth = GraphicsDevice.DisplayMode.Width;
+			//screenHeight = GraphicsDevice.DisplayMode.Height;
+
+			screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height * screenscale;
+			screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width * screenscale;
+
+			//float scale = ScaleToHighDPI(screenWidth);
+
+			//Console.WriteLine("Width = " + screenWidth);
+			//this.Window.AllowUserResizing = true;
+			//this.Window.ClientSizeChanged += new EventHandler<EventArgs>(Window_ClientSizeChanged);
+
+
+			//graphics.PreferredBackBufferHeight =  900 * (int) screenscale;
+			//graphics.PreferredBackBufferWidth = 1440 * (int) screenscale;
+			//graphics.ApplyChanges();
+			base.Initialize();
+		}
+
+		public float ScaleToHighDPI(int width)
+		{
+			//int trueWidth = this.GraphicsDevice.Viewport.Width;
+			int trueWidth = Window.ClientBounds.Width;
+			Console.WriteLine(trueWidth);
+
+			int trueHeight = this.GraphicsDevice.Viewport.Height;
+
+
+			float ret = trueWidth / width;
+			Console.WriteLine(ret);
+			//DisplayInformation d = DisplayInformation.GetForCurrentView();
+			//f *= (float)d.RawPixelsPerViewPixel;
+			return ret;
+		}
+
+
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			//grass = Content.Load<Texture2D>("grass-0");
+
+			tile = new SpriteClass(this, "grass-0", 300 * (int) screenscale, 300 * (int) screenscale, 3f * screenscale);
+		}
+
+
+		protected override void Update(GameTime gameTime)
+		{
+			
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, null, null, null);
+
+			//spriteBatch.Draw(grass, new Vector2(100, 100));
+			tile.Draw(spriteBatch);
+
+			spriteBatch.End();
+
+
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ SpriteClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace TileCombat
+{
+	public class SpriteClass : DrawableGameComponent 
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int angle { get; set; }
+		public float scale { get; set; }
+		public int dX { get; set; }
+		public int dY { get; set; }
+		public int dAngle { get; set; }
+		public float dScale { get; set; }
+		public Texture2D texture { get; }
+
+		public SpriteClass(Game game,  string textureName, int X, int Y, float scale) : base(game)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.scale = scale;
+
+			// Load the specified texture
+			texture = game.Content.Load<Texture2D>(textureName);
+		}
+
+		//public void LoadContent()
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			// Determine the position vector of the sprite
+			Vector2 spritePosition = new Vector2(this.X, this.Y);
+
+			// Draw the sprite
+			spriteBatch.Draw(texture, spritePosition, null, Color.White, this.angle, new Vector2(texture.Width / 2, texture.Height / 2), new Vector2(scale, scale), SpriteEffects.None, 0f);
+		}
+
+	}
+}
